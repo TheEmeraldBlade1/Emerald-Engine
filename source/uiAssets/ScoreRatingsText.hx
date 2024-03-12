a871dc54c09bade25ea61930e1fd3275053b6c3f
Emerald <87787233+TheEmeraldBlade1@users.noreply.github.com>
2024-03-12 16:39:58 -0700

    2.0.0 ready

--- source/uiAssets/ScoreRatingsText.hx
+++ source/uiAssets/ScoreRatingsText.hx
@@ -10,20 +10,29 @@ class ScoreRatingsText{
     public static var instance:ScoreRatingsText;
     public static var game:PlayState;
 
-    public static var accuracyString:Array<String> = ['Score: ', 'Misses: ', 'Rating: ', ' | ', '%'];
+    public static var accuracyString:Array<String> = ['Score: ', 'Misses: ', 'Rating: ', ' | ', '%', '0', '?'];
 
     public function getFont(){
         return Paths.font("vcr.ttf");
     }
     public function firstScore(){
-        return "Score: 0 | Misses: 0 | Rating: ?";
+        return accuracyString[0]
+        + accuracyString[5]
+        + accuracyString[3] 
+        + accuracyString[1]
+        + accuracyString[5]
+        + accuracyString[3] 
+        + accuracyString[2] 
+        + accuracyString[6];
     }
     public function getScore(score:Int, misses:Int, percent:Float){
         return accuracyString[0] + score 
         + accuracyString[3] 
-        + accuracyString[1] + misses 
+        + accuracyString[1] 
+        + misses 
         + accuracyString[3] 
-        + accuracyString[2] + Math.floor(percent * 100) 
+        + accuracyString[2] 
+        + Math.floor(percent * 100) 
         + accuracyString[4];
     }
 }
